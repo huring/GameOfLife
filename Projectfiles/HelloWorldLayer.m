0f0c91cebd0724cc10e85b13f016568282280966
Lars Huring <lars.huring@loveatwork.se>
2014-01-14 23:37:23 +0100

    Initial import

---
+++ Projectfiles/HelloWorldLayer.m
@@ -0,0 +1,29 @@
+/*
+ * Kobold2D™ --- http://www.kobold2d.org
+ *
+ * Copyright (c) 2010-2011 Steffen Itterheim. 
+ * Released under MIT License in Germany (LICENSE-Kobold2D.txt).
+ */
+
+#import "HelloWorldLayer.h"
+#import "SimpleAudioEngine.h"
+
+@interface HelloWorldLayer (PrivateMethods)
+@end
+
+@implementation HelloWorldLayer
+
+@synthesize helloWorldString, helloWorldFontName;
+@synthesize helloWorldFontSize;
+
+-(id) init
+{
+	if ((self = [super init]))
+	{
+		
+	}
+
+	return self;
+}
+
+@end
